2364306b201896dc0ec3587bdadfdafdc5630f0c
Ali Umar <ali.umar.work@gmail.com>
2023-08-31 23:26:40 -0400

    moved some code around lul

--- src/main.c
+++ src/main.c
@@ -1,83 +1,55 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Keyboard input
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2014-2023 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
+typedef struct _Ball {
+  Vector2 pos;
+  Vector2 vel;
+  float radius;
+} Ball;
 
-    Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+const int screenWidth  = 800;
+const int screenHeight = 450;
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-    //Set a ball radius
-    int ballRadius = 50;
+int main(void) {
 
-    //--------------------------------------------------------------------------------------
-    //Setting Max boundaries so that ball does not go out of window
-    float xMax = screenWidth - ballRadius;
-    float xMin = 0 + ballRadius;
-    float yMax = screenHeight - ballRadius; 
-    float yMin = 0 + ballRadius;
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
+    InitWindow(screenWidth, screenHeight, "2D Platformer Game");
+    SetTargetFPS(60);               
+
+    Ball ball;
+    init(&ball); 
+    float acc = 0.0f;
+    while (!WindowShouldClose()) {
         
-        // Update
-        //----------------------------------------------------------------------------------
-        if (IsKeyDown(KEY_RIGHT) && ballPosition.x <= xMax){ 
-            ballPosition.x += 2.0f;
-        }
-        if (IsKeyDown(KEY_LEFT) && ballPosition.x >= xMin){
-            ballPosition.x -= 2.0f;
-        } 
-        if (IsKeyDown(KEY_UP) && ballPosition.y >= yMin) {
-            ballPosition.y -= 2.0f;
-        }
-        if (IsKeyDown(KEY_DOWN) && ballPosition.y <= yMax){ 
-            ballPosition.y += 2.0f;
-        }
+        float dt = GetFrameTime();
+        printf("dt: %f acc: %f\n", dt, acc);
 
-        if (IsKeyDown(KEY_SPACE))  ballPosition.y -= 10.0f;
-        
-        //----------------------------------------------------------------------------------
-        
-        // Draw
-        //----------------------------------------------------------------------------------
+        acc += dt;
+        handleKeypress(&ball);
+
         BeginDrawing();
-
-            ClearBackground(RAYWHITE);  
-
-            DrawText("hello", 10, 10, 20, DARKGRAY);
-
-            DrawCircleV(ballPosition, ballRadius, MAROON);
-
+        ClearBackground(RAYWHITE);  
+        DrawText("hello", 10, 10, 20, DARKGRAY);
+        DrawCircleV(ball.pos, ball.radius, MAROON);
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
     return 0;
+}
+
+void init(Ball* ball) { 
+   ball->pos = (Vector2){ (float) screenWidth / 2, (float) screenHeight / 2 };  
+   ball->vel = (Vector2){ 0.0f, 0.0f };  
+   ball->radius = 50;
+}
+
+void handleKeypress(Ball* ball) {
+    float xMax = screenWidth - ball->radius;
+    float xMin = 0 + ball->radius;
+    float yMax = screenHeight - ball->radius; 
+    float yMin = 0 + ball->radius;
+
+    if (IsKeyDown(KEY_RIGHT) && ball->pos.x <= xMax) ball->pos.x += 2.0f; 
+    if (IsKeyDown(KEY_LEFT) && ball->pos.x >= xMin)  ball->pos.x -= 2.0f;  
+    if (IsKeyDown(KEY_UP) && ball->pos.y >= yMin)    ball->pos.y -= 2.0f; 
+    if (IsKeyDown(KEY_DOWN) && ball->pos.y <= yMax)  ball->pos.y += 2.0f; 
+    if (IsKeyDown(KEY_SPACE)) ball->pos.y -= 10.0f;
 }
